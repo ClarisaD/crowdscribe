48fe7b571b4a033a74c9069dc856da4e7b89cdca
Darius Bacon <darius@wry.me>
2016-06-26 14:35:29 -0400

    updated DB query sketch

--- dbmessing.js
+++ dbmessing.js
@@ -19,8 +19,11 @@ console.log('starting')
 // XXX untested
 function findProgenitor(url, callback) {
     function climbTheFamilyTree(err, media) {
+        console.log("climbing", err, media);
         if (err) return callback(err);
-        if (media.parentId === null) {  // XXX or should we check for "" instead?
+        if (media === null) {
+            return callback(null, null);
+        } else if (media.parentId === null) {  // XXX or should we check for "" instead?
             // We have a progenitor.
             return callback(null, media);
         } else {
@@ -30,40 +33,71 @@ function findProgenitor(url, callback) {
             Media.findOne({mediaId: media.parentId}, climbTheFamilyTree);
         }
     }
+    console.log('starting the climb', url);
     Media.findOne({url: url}, climbTheFamilyTree);
 }
 
 function tellMeAbout(url, callback) {
-    Request.find({media: url}, (err, requests) => {
+    findProgenitor(url, (err, media) => {
         if (err) return callback(err);
-
-        // (this is probably wrong in that mediaId is not the url?)
-        // - Yeah, you have to ping the db to get the mediaId. - Stan
-
-        Transcript.find({mediaId: url}, (err, transcripts) => {
+        if (media === null) return callback(null, {
+            numRequests: 0,
+            transcriptText: null,
+            transcriptScore: 0,
+        });
+        Request.count({media: media.mediaId}, (err, numRequests) => {
             if (err) return callback(err);
-            let nTranscripts = transcripts.length;
+            Transcript.find({mediaId: media.mediaId}, (err, transcripts) => {
+                if (err) return callback(err);
+                let nRemainingTranscripts = transcripts.length;
 
-            // I don't quite understand what you're doing here.
-            // You are making an array that stores all the
-            // transcript objects for later referral? - Stan
-
-            const transcriptInfo = new Map();
-            transcripts.forEach(transcript => {
-                const transcriptId = transcript.mediaId + ',' + transcript.userId; // XXX or what?
-                Feedback.find({transcriptId: transcriptId}, (err, feedbacks) => {
-                    if (err) return callback(err);
-                    transcriptInfo.set(transcriptId, feedbacks);
-                    --nTranscripts;
-                    if (nTranscripts === 0) {
-                        callback(null, {requests, transcriptInfo});
-                    }
+                const transcriptInfo = new Map();
+                transcripts.forEach(transcript => {
+                    Feedback.find({transcriptId: transcript._id}, (err, feedbacks) => {
+                        if (err) return callback(err);
+                        transcriptInfo.set(transcriptId, {transcript, feedbacks});
+                        --nRemainingTranscripts;
+                        if (nRemainingTranscripts === 0) {
+                            const pick = pickATranscript(transcriptInfo);
+                            callback(null, {numRequests, transcriptText: pick.transcriptText, transcriptScore: pick.transcriptScore});
+                        }
+                    });
                 });
             });
         });
     });
 }
 
+function pickATranscript(transcriptMap) {
+    var bestScore, bestText;
+    transcriptMap.forEach((id, value) => {
+        const transcript = value.transcript;
+        const feedbacks = value.feedbacks;
+        const score = totalScore(feedbacks);
+        if (bestScore === void 0 || bestScore < score) {
+            bestScore = score;
+            bestText = transcript.content;
+        }
+    });
+    return {transcriptText: bestText,
+            transcriptScore: bestScore};
+}
+
+function totalScore(feedbacks) {
+    const count = feedbacks.length;
+    let sum = feedbacks.reduce((accum, feedback) => accum + oneScore(feedback),
+                               0);
+    return 100 * (0 < count ? sum / count : 0);   // TODO: give more credence when there are more ratings...
+}
+
+function oneScore(feedback) {
+    switch (feedback.type) {   // XXX I don't really know what we're going to put in feedback.type...
+    case '+1': return 1;
+    case '-1': return -1;
+    case 'spam': return -7;
+    default: console.log("Bad feedback.type", feedback); return 0;
+    }
+}
 
 if (0) {
     var t = new Transcript({
@@ -92,4 +126,4 @@ if (0) {
 }
 
 
-console.log('started')
+console.log('started');

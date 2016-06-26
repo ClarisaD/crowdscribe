083066f9203a42e94b08aa41e8a378f94538592a
Darius Bacon <darius@wry.me>
2016-06-26 13:25:48 -0400

    findProgenitor function

--- dbmessing.js
+++ dbmessing.js
@@ -16,6 +16,23 @@ mongoose.connect(dbConfig.dbUrl);
 
 console.log('starting')
 
+// XXX untested
+function findProgenitor(url, callback) {
+    function climbTheFamilyTree(err, media) {
+        if (err) return callback(err);
+        if (media.parentId === null) {  // XXX or should we check for "" instead?
+            // We have a progenitor.
+            return callback(null, media);
+        } else {
+            // Maybe we could assume the immediate parent is as far
+            // back as the ancestry goes? But here I'm not making that
+            // assumption.
+            Media.findOne({mediaId: media.parentId}, climbTheFamilyTree);
+        }
+    }
+    Media.findOne({url: url}, climbTheFamilyTree);
+}
+
 function tellMeAbout(url, callback) {
     // is this how the schema is meant to work?
 

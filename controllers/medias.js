cbb25d2ec0745d7180e58432ff214cd4df27cde1
Darius Bacon <darius@wry.me>
2016-06-26 15:36:44 -0400

    reorg and comment

--- controllers/medias.js
+++ controllers/medias.js
@@ -16,6 +16,7 @@ router.get('/json', function (req, res) {
   var url = req.query.url;
   tellMeAbout(url, urlInfo => {
     // TODO: return real data here from urlInfo
+    // res.json(urlInfo);
     res.json({ url      : url,
                numRequests : 0,
                transcriptText: "This is the dummy transcript",
@@ -23,6 +24,40 @@ router.get('/json', function (req, res) {
   });
 });
 
+function tellMeAbout(url, callback) {
+  findProgenitor(url, (err, media) => {
+    if (err) return callback(err);
+    if (media === null) return callback(null, {
+      url: url,
+      numRequests: 0,
+      transcriptText: null,
+      transcriptScore: 0,
+    });
+    Request.count({media: media.mediaId}, (err, numRequests) => {
+      if (err) return callback(err);
+      Transcript.find({mediaId: media.mediaId}, (err, transcripts) => {
+        if (err) return callback(err);
+        let nRemainingTranscripts = transcripts.length;
+        const transcriptInfo = new Map();
+        transcripts.forEach(transcript => {
+          Feedback.find({transcriptId: transcript._id}, (err, feedbacks) => {
+            if (err) return callback(err);
+            transcriptInfo.set(transcriptId, {transcript, feedbacks});
+            --nRemainingTranscripts;
+            if (nRemainingTranscripts === 0) {
+              const pick = pickATranscript(transcriptInfo);
+              callback(null, {url, numRequests,
+                              transcriptText: pick.transcriptText,
+                              transcriptScore: pick.transcriptScore});
+            }
+          });
+        });
+      });
+    });
+  });
+}
+
+// Call callback with the media record best representing url.
 function findProgenitor(url, callback) {
   function climbTheFamilyTree(err, media) {
     console.log("climbing", err, media);
@@ -43,40 +78,7 @@ function findProgenitor(url, callback) {
   Media.findOne({url: url}, climbTheFamilyTree);
 }
 
-function tellMeAbout(url, callback) {
-  findProgenitor(url, (err, media) => {
-    if (err) return callback(err);
-    if (media === null) return callback(null, {
-      url: url,
-      numRequests: 0,
-      transcriptText: null,
-      transcriptScore: 0,
-    });
-    Request.count({media: media.mediaId}, (err, numRequests) => {
-      if (err) return callback(err);
-      Transcript.find({mediaId: media.mediaId}, (err, transcripts) => {
-        if (err) return callback(err);
-        let nRemainingTranscripts = transcripts.length;
-
-        const transcriptInfo = new Map();
-        transcripts.forEach(transcript => {
-          Feedback.find({transcriptId: transcript._id}, (err, feedbacks) => {
-            if (err) return callback(err);
-            transcriptInfo.set(transcriptId, {transcript, feedbacks});
-            --nRemainingTranscripts;
-            if (nRemainingTranscripts === 0) {
-              const pick = pickATranscript(transcriptInfo);
-              callback(null, {url, numRequests,
-                              transcriptText: pick.transcriptText,
-                              transcriptScore: pick.transcriptScore});
-            }
-          });
-        });
-      });
-    });
-  });
-}
-
+// Return text and score of the 'best' transcript in the map.
 function pickATranscript(transcriptMap) {
   var bestScore, bestText;
   transcriptMap.forEach((id, value) => {
@@ -92,6 +94,7 @@ function pickATranscript(transcriptMap) {
           transcriptScore: bestScore};
 }
 
+// Given an array of feedback records, return a numerical overall score (higher is better).
 function totalScore(feedbacks) {
   const count = feedbacks.length;
   let sum = feedbacks.reduce((accum, feedback) => accum + oneScore(feedback),
@@ -99,6 +102,7 @@ function totalScore(feedbacks) {
   return 100 * (0 < count ? sum / count : 0);   // TODO: give more credence when there are more ratings...
 }
 
+// Score a single feedback record.
 function oneScore(feedback) {
   switch (feedback.type) {   // XXX I don't really know what we're going to put in feedback.type...
   case '+1': return 1;

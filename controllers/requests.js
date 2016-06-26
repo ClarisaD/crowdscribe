d9d759e1607dbae0ca7184d763052ca15dc343ee
Stanley Sakai <staniel@uw.edu>
2016-06-26 11:39:44 -0400

    need to actually comment stuff.

--- controllers/requests.js
+++ controllers/requests.js
@@ -7,10 +7,13 @@ module.exports = function (app) {
     var newMediaURL = req.body.url; //
     var newMediaID  = newMedia._id;
 
+    // saves new media object.
     newMedia.save(function (err, media) {
       if (err) {
         res.send(err);
       } else {
+        // render the media page, passing in the original
+        // url of the media.
         res.render(302, '/media', { 'url' : newMediaURL });
       }
     });

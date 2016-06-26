ccbedf52627ebc21f8b7ad61b9af5223edfba48b
Stanley Sakai <staniel@uw.edu>
2016-06-26 12:32:10 -0400

    got rid of morgan dammit

--- controllers/medias.js
+++ controllers/medias.js
@@ -2,10 +2,6 @@ module.exports = function(app, passport) {
 
   var Media = require('../models/media.js');
 
-  // morgan is logger middleware. it shows
-  // stuff going on in the background.
-  app.use(morgan('dev'));
-
   // load summary page based on URL.
   app.get('/', function (req, res) {
       res.render('/media/index', { url : req.query.url });
